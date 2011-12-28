fe7177b330f45b4ca33b06ddd942331939c456e6
aki <aki@xps>
2011-12-28 14:48:00 +0900

    test1

--- src/com/akjava/gwt/modelweight/client/GWTModelWeight.java
+++ src/com/akjava/gwt/modelweight/client/GWTModelWeight.java
@@ -6,14 +6,21 @@ import com.akjava.bvh.client.BVH;
 import com.akjava.bvh.client.BVHParser;
 import com.akjava.bvh.client.BVHParser.ParserListener;
 import com.akjava.gwt.three.client.THREE;
+import com.akjava.gwt.three.client.core.Geometry;
 import com.akjava.gwt.three.client.core.Matrix4;
 import com.akjava.gwt.three.client.core.Quaternion;
 import com.akjava.gwt.three.client.core.Vector3;
+import com.akjava.gwt.three.client.extras.GeometryUtils;
+import com.akjava.gwt.three.client.extras.loaders.JSONLoader;
+import com.akjava.gwt.three.client.extras.loaders.JSONLoader.LoadHandler;
 import com.akjava.gwt.three.client.gwt.SimpleDemoEntryPoint;
 import com.akjava.gwt.three.client.gwt.animation.AnimationBone;
 import com.akjava.gwt.three.client.gwt.animation.AnimationData;
 import com.akjava.gwt.three.client.gwt.animation.AnimationHierarchyItem;
 import com.akjava.gwt.three.client.gwt.animation.AnimationKey;
+import com.akjava.gwt.three.client.gwt.animation.AnimationUtils;
+import com.akjava.gwt.three.client.gwt.model.JSONModelFile;
+import com.akjava.gwt.three.client.gwt.model.MetaData;
 import com.akjava.gwt.three.client.lights.Light;
 import com.akjava.gwt.three.client.objects.Mesh;
 import com.akjava.gwt.three.client.renderers.WebGLRenderer;
@@ -28,6 +35,7 @@ import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Panel;
 
@@ -46,14 +54,51 @@ public class GWTModelWeight extends SimpleDemoEntryPoint{
 	@Override
 	protected void initializeOthers(WebGLRenderer renderer) {
 
+		//write test
+		Geometry g=THREE.CubeGeometry(1, 1, 1);
+		log(g);
+		
+		Geometry g2=THREE.CubeGeometry(1, 1, 1);
+		Matrix4 mx=THREE.Matrix4();
+		mx.setPosition(THREE.Vector3(0,10,0));
+		//g2.applyMatrix(mx);
+		Mesh tmpM=THREE.Mesh(g2, THREE.MeshBasicMaterial().build());
+		tmpM.setPosition(0, 10, 0);
+		
+		
+		GeometryUtils.merge(g, tmpM);
+		
+		JSONModelFile model=JSONModelFile.create();
+		
+		model.setVertices(g.vertices());
+		model.setFaces(g.faces());
+		
+		
+		
+		//JSONArray vertices=new JSONArray(nums);
+		JSONObject js=new JSONObject(model);
+		log(js.toString());
 		
 		scene.add(THREE.AmbientLight(0x888888));
 		Light pointLight = THREE.PointLight(0xffffff);
 		pointLight.setPosition(0, 10, 300);
 		scene.add(pointLight);
 		
-		loadBVH("14_08.bvh");
+		//loadBVH("14_08.bvh");
+		JSONLoader loader=THREE.JSONLoader();
+		loader.load("test.json", new LoadHandler() {
+			
+			@Override
+			public void loaded(Geometry geometry) {
+				log(geometry);
+				Mesh mesh=THREE.Mesh(geometry, THREE.MeshLambertMaterial().color(0xff0000).build());
+				scene.add(mesh);
+			}
+		});
+		
 	}
+	
+	
 
 	@Override
 	public void onMouseClick(ClickEvent event) {
@@ -124,18 +169,34 @@ public void onError(Request request, Throwable exception) {
 					mesh.setPosition(pos.get(0),pos.get(1),pos.get(2));
 				}
 				
-				int  keyIndex=2;
+				int  keyIndex=5;
 				AnimationDataConverter dataConverter=new AnimationDataConverter();
 				AnimationData data=dataConverter.convertJsonAnimation(bvh);
 				log(data);
 				JsArray<AnimationHierarchyItem> hitem=data.getHierarchy();
 				for(int i=0;i<hitem.length();i++){
 					AnimationHierarchyItem item=hitem.get(i);
+					
+					AnimationHierarchyItem parent=null;
+					if(item.getParent()!=-1){
+						parent=hitem.get(item.getParent());
+					}
 					AnimationKey key=item.getKeys().get(keyIndex);
 					Mesh mesh=THREE.Mesh(THREE.CubeGeometry(.5, .5, .5), THREE.MeshLambertMaterial().color(0x00ff00).build());
 					scene.add(mesh);
-					JsArrayNumber pos=key.getPos();
-					mesh.setPosition(pos.get(0),pos.get(1),pos.get(2));
+					
+					Vector3 meshPos=AnimationUtils.getPosition(key);
+					mesh.setPosition(meshPos);
+					if(parent!=null){
+						Vector3 parentPos=AnimationUtils.getPosition(parent.getKeys().get(keyIndex));
+						Geometry lineG = THREE.Geometry();
+						lineG.vertices().push(THREE.Vertex(meshPos));
+						lineG.vertices().push(THREE.Vertex(parentPos));
+						Mesh line=THREE.Line(lineG, THREE.LineBasicMaterial().color(0xffffff).build());
+						scene.add(line);
+					}
+					
+					
 					
 					Quaternion q=key.getRot();
 					Matrix4 rot=THREE.Matrix4();
