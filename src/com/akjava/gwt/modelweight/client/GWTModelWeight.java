d75439e882b119b6f7aba82424f2f7f25c636d64
aki <aki@xps>
2011-12-29 08:58:24 +0900

    boxman worked

--- src/com/akjava/gwt/modelweight/client/GWTModelWeight.java
+++ src/com/akjava/gwt/modelweight/client/GWTModelWeight.java
@@ -1,27 +1,32 @@
 package com.akjava.gwt.modelweight.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.akjava.bvh.client.AnimationBoneConverter;
 import com.akjava.bvh.client.AnimationDataConverter;
 import com.akjava.bvh.client.BVH;
 import com.akjava.bvh.client.BVHParser;
 import com.akjava.bvh.client.BVHParser.ParserListener;
+import com.akjava.gwt.html5.client.HTML5InputRange;
+import com.akjava.gwt.html5.client.extra.HTML5Builder;
 import com.akjava.gwt.three.client.THREE;
 import com.akjava.gwt.three.client.core.Geometry;
 import com.akjava.gwt.three.client.core.Matrix4;
-import com.akjava.gwt.three.client.core.Quaternion;
+import com.akjava.gwt.three.client.core.Object3D;
 import com.akjava.gwt.three.client.core.Vector3;
 import com.akjava.gwt.three.client.core.Vector4;
+import com.akjava.gwt.three.client.extras.GeometryUtils;
 import com.akjava.gwt.three.client.extras.animation.Animation;
 import com.akjava.gwt.three.client.extras.animation.AnimationHandler;
 import com.akjava.gwt.three.client.extras.loaders.JSONLoader;
 import com.akjava.gwt.three.client.extras.loaders.JSONLoader.LoadHandler;
 import com.akjava.gwt.three.client.gwt.Clock;
+import com.akjava.gwt.three.client.gwt.GWTGeometryUtils;
 import com.akjava.gwt.three.client.gwt.SimpleDemoEntryPoint;
 import com.akjava.gwt.three.client.gwt.animation.AnimationBone;
 import com.akjava.gwt.three.client.gwt.animation.AnimationData;
 import com.akjava.gwt.three.client.gwt.animation.AnimationHierarchyItem;
-import com.akjava.gwt.three.client.gwt.animation.AnimationKey;
-import com.akjava.gwt.three.client.gwt.animation.AnimationUtils;
 import com.akjava.gwt.three.client.lights.Light;
 import com.akjava.gwt.three.client.objects.Mesh;
 import com.akjava.gwt.three.client.objects.SkinnedMesh;
@@ -30,6 +35,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsArrayNumber;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
@@ -40,6 +46,8 @@ import com.google.gwt.http.client.URL;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
 
 
@@ -50,6 +58,10 @@ public class GWTModelWeight extends SimpleDemoEntryPoint{
 
 	@Override
 	protected void beforeUpdate(WebGLRenderer renderer) {
+		if(root!=null){
+			root.getRotation().set(Math.toRadians(rotationRange.getValue()),Math.toRadians(rotationYRange.getValue()),Math.toRadians(rotationZRange.getValue()));
+			}
+		
 		long delta=clock.delta();
 		//log(""+animation.getCurrentTime());
 		double v=(double)delta/1000;
@@ -120,13 +132,18 @@ public class GWTModelWeight extends SimpleDemoEntryPoint{
 					weight.push(v4w);
 				}
 				
+				
+				
+				
 				cube.setSkinIndices(indices);
 				cube.setSkinWeight(weight);
 				
 				cube.setBones(geometry.getBones());
 				
-				SkinnedMesh mesh=THREE.SkinnedMesh(cube, THREE.MeshLambertMaterial().skinning(true).color(0xff0000).build());
-				scene.add(mesh);
+				//root=boneToCube(geometry.getBones());
+				//scene.add(root);
+				//SkinnedMesh mesh=THREE.SkinnedMesh(cube, THREE.MeshLambertMaterial().skinning(true).color(0xff0000).build());
+				//scene.add(mesh);
 				GWT.log("l2.5");
 				//Animation animation = THREE.Animation( mesh, "take_001" );
 				//log(animation);
@@ -137,6 +154,39 @@ public class GWTModelWeight extends SimpleDemoEntryPoint{
 		});
 		
 		loadBVH("14_08.bvh");
+	}
+	Object3D root;
+	
+	List<Mesh> tmp=new ArrayList<Mesh>();
+	private Object3D boneToCube(JsArray<AnimationBone> bones){
+		tmp.clear();
+		Object3D group=THREE.Object3D();
+		for(int i=0;i<bones.length();i++){
+			AnimationBone bone=bones.get(i);
+			Geometry cube=THREE.CubeGeometry(1, 1, 1);
+			int color=0xff0000;
+			if(i==0){
+				color=0x00ff00;
+			}
+			Mesh mesh=THREE.Mesh(cube, THREE.MeshLambertMaterial().color(color).build());
+			group.add(mesh);
+			Vector3 pos=AnimationBone.jsArrayToVector3(bone.getPos());
+			
+			if(bone.getParent()!=-1){
+				Vector3 ppos=tmp.get(bone.getParent()).getPosition();
+				pos.addSelf(ppos);
+			}
+			mesh.setPosition(pos);
+			
+			if(bone.getParent()!=-1){
+				//AnimationBone parent=bones.get(bone.getParent());
+				Vector3 ppos=tmp.get(bone.getParent()).getPosition();
+				Mesh line=THREE.Line(GWTGeometryUtils.createLine(pos, ppos), THREE.LineBasicMaterial().color(0x888888).build());
+				group.add(line);
+			}
+			tmp.add(mesh);
+		}
+		return group;
 	}
 	
 	
@@ -152,11 +202,55 @@ public class GWTModelWeight extends SimpleDemoEntryPoint{
 		// TODO Auto-generated method stub
 		
 	}
-
+	private HTML5InputRange rotationRange;
+	private HTML5InputRange rotationYRange;
+	private HTML5InputRange rotationZRange;
 	@Override
 	public void createControl(Panel parent) {
-		// TODO Auto-generated method stub
+HorizontalPanel h1=new HorizontalPanel();
 		
+		rotationRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("X-Rotate:", rotationRange));
+		parent.add(h1);
+		h1.add(rotationRange);
+		Button reset=new Button("Reset");
+		reset.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationRange.setValue(0);
+			}
+		});
+		h1.add(reset);
+		
+		HorizontalPanel h2=new HorizontalPanel();
+		
+		rotationYRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("Y-Rotate:", rotationYRange));
+		parent.add(h2);
+		h2.add(rotationYRange);
+		Button reset2=new Button("Reset");
+		reset2.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationYRange.setValue(0);
+			}
+		});
+		h2.add(reset2);
+		
+		
+		HorizontalPanel h3=new HorizontalPanel();
+		rotationZRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("Z-Rotate:", rotationZRange));
+		parent.add(h3);
+		h3.add(rotationZRange);
+		Button reset3=new Button("Reset");
+		reset3.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationZRange.setValue(0);
+			}
+		});
+		h3.add(reset3);
 	}
 	
 	private void loadBVH(String path){
@@ -209,7 +303,7 @@ public void onError(Request request, Throwable exception) {
 				AnimationBoneConverter converter=new AnimationBoneConverter();
 				JsArray<AnimationBone> bones=converter.convertJsonBone(bvh);
 				for(int i=0;i<bones.length();i++){
-					Mesh mesh=THREE.Mesh(THREE.CubeGeometry(.5, .5, .5), THREE.MeshLambertMaterial().color(0xff0000).build());
+					Mesh mesh=THREE.Mesh(THREE.CubeGeometry(.5, .5, .5), THREE.MeshLambertMaterial().color(0x0000ff).build());
 					scene.add(mesh);
 					JsArrayNumber pos=bones.get(i).getPos();
 					mesh.setPosition(pos.get(0),pos.get(1),pos.get(2));
@@ -271,10 +365,42 @@ public void onError(Request request, Throwable exception) {
 					weight.push(v4w);
 				}
 				
+				
+				List<Vector3> parentPos=new ArrayList<Vector3>();
+				parentPos.add(THREE.Vector3());
+				
+				for(int j=1;j<bones.length();j++){
+					Geometry cbone=THREE.CubeGeometry(1, 1, 1);
+					AnimationBone ab=bones.get(j);
+					Vector3 pos=AnimationBone.jsArrayToVector3(ab.getPos());
+					pos.addSelf(parentPos.get(ab.getParent()));
+					parentPos.add(pos);
+					
+					Matrix4 m4=THREE.Matrix4();
+					m4.setPosition(pos);
+					cbone.applyMatrix(m4);
+					GeometryUtils.merge(cube, cbone);
+					for(int i=0;i<cbone.vertices().length();i++){
+						Vector4 v4=THREE.Vector4();
+						v4.set(j, j, 0, 0);
+					//	v4.set(0, 0, 0, 0);
+						indices.push(v4);
+						
+						Vector4 v4w=THREE.Vector4();
+						v4w.set(1, 0, 0, 0);
+						weight.push(v4w);
+					}
+				}
+				
+				
+				log("cube");
+				log(cube);
+				
 				cube.setSkinIndices(indices);
 				cube.setSkinWeight(weight);
 				
 				cube.setBones(bones);
+				
 				AnimationHandler.add(data);
 				log(data);
 				log(bones);
@@ -285,11 +411,16 @@ public void onError(Request request, Throwable exception) {
 				JSONObject test=new JSONObject(data);
 				//log(test.toString());
 				
+				root=boneToCube(bones);
+				scene.add(root);
+				
 				
 				log("before create");
 				SkinnedMesh mesh=THREE.SkinnedMesh(cube, THREE.MeshLambertMaterial().skinning(true).color(0xff0000).build());
+				//Mesh mesh=THREE.Mesh(cube, THREE.MeshLambertMaterial().skinning(false).color(0xff0000).build());
 				log("create-mesh");
 				scene.add(mesh);
+				
 				
 				animation = THREE.Animation( mesh, data.getName() );
 				log(animation);
